f3b6fe510ea432ccd3e3a24ac9baaf4b4d85fc5d
Vinicius Macena <113395429+ViniciusMacena@users.noreply.github.com>
2023-07-11 18:52:30 -0300

    Add files via upload

---
+++ Desafio/Desafio01 .playground/Contents.swift
@@ -0,0 +1,19 @@
+// Desafio 01 - Tela quente
+print("Desafio 01")
+let celsius = 30.2
+let calculoFahrenheit = (celsius * 9 / 5) + 32
+
+print("Em fahrenheit, a temperatura é de \(calculoFahrenheit)ºF.")
+print()
+
+//Desafio 02 - Cálculo de Média
+
+print("Desafio 02")
+let nota01 = 5.5
+let nota02 = 7.7
+let nota03 = 8.5
+
+let media = (nota01 + nota02 + nota03) / 3
+
+print("A media do aluno foi de: \(media)")
+
